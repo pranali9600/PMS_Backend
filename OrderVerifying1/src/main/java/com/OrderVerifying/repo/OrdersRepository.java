ded2e5cd7e494fbf5067454aebd8b3241c1c5f91
Pranali Redgaonkar <71374016+pranali9600@users.noreply.github.com>
2023-01-22 23:47:41 +0530

    Delete OrderVerifying1 directory

--- OrderVerifying1/src/main/java/com/OrderVerifying/repo/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.OrderVerifying.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.OrderVerifying.models.Orders;
-
-public interface OrdersRepository extends MongoRepository<Orders, String>{
-
-}
